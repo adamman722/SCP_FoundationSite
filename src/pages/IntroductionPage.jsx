afa06cd0eedb791da9e9036d47ed25a1f6955f09
adamman722 <tarpeyadam@gmail.com>
2022-02-14 21:30:52 -0500

    Added cool typewriter effects

--- src/pages/IntroductionPage.jsx
+++ src/pages/IntroductionPage.jsx
@@ -3,12 +3,40 @@ import Typewriter from "typewriter-effect";
 
 function IntroductionPage() {
   return (
-    <div style={{ color: "white" }} className="App">
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter.typeString("Hello Dr.■■■■■■").start();
-        }}
-      />
+    <div style={{ color: "#39FF14" }} className="App">
+      <div className="typewriter1">
+        <Typewriter
+          className="nameeee"
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(75)
+              .typeString("Hello Dr.■■■■■■")
+              .pauseFor(500)
+              .typeString(".")
+              .pauseFor(500)
+              .typeString(".")
+              .pauseFor(500)
+              .typeString(".")
+              .start()
+              .pauseFor(7000)
+              .deleteAll();
+          }}
+        />
+      </div>
+      <div className="typewriter2">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(75)
+              .pauseFor(3300)
+              .typeString("...We have been waiting for you")
+              .start()
+              .pauseFor(2000)
+              .deleteAll();
+          }}
+          className="typewriter2"
+        />
+      </div>
     </div>
   );
 }
